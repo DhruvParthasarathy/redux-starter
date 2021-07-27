c0896517a3f301652a95fe207be30298b66e923a
Dhruv.Parthasarathy <dhruv.parthasarathy@mu-sigma.com>
2021-07-27 15:14:57 +0530

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import store from './store'
 
-store.subscribe( () => {
-    console.log("Store changed", store.getState())
+/**
+ * the subscribe method of the store acts as a listener to the store and whenever the store's state changes, this function defined inside it gets triggered, a console.log in this case
+ */
+
+// store.subscribe( () => {
+//     console.log("Store changed", store.getState())
+// })
+
+/**
+ * store.subscribe also returns a function which when called removes the listener from the store, thereby preventing durther dispatch events from being listened to, but the dispatch event is still processed
+ */
+const unsubscribe = store.subscribe( () => {
+    console.log("Store change listened ===========>", store.getState())
 })
+
 store.dispatch(
     {
         type: "bugAdded",
@@ -12,6 +24,12 @@ store.dispatch(
     }
 )
 
+unsubscribe()
+
+
+/**
+ * Given that we have unsubscribed above, the below dispatch event will happen, but the callback of the subscribe method will not be called
+ */
 store.dispatch(
     {
         type: 'bugRemoved',
